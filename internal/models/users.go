7e7c2fb19aff1c8229e63f6d3245006540e9edaf
Kirin <rudenokaleksandr21@gmail.com>
2024-09-12 13:24:39 +0300

    testing

--- internal/models/users.go
+++ internal/models/users.go
@@ -22,6 +22,12 @@ type UserModel struct {
 	DB *sql.DB
 }
 
+type UserModelInterface interface {
+	Insert(name, email, password string) error
+	Authenticate(email, password string) (int, error)
+	Exists(id int) (bool, error)
+}
+
 func (m *UserModel) Insert(name, email, password string) error {
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
 	if err != nil {
@@ -73,7 +79,7 @@ func (m *UserModel) Authenticate(email, password string) (int, error) {
 	return id, nil
 }
 
-func (m *UserModel) Exist(id int) (bool, error) {
+func (m *UserModel) Exists(id int) (bool, error) {
 	var exists bool
 
 	stmt := `SELECT EXISTS(SELECT true FROM users WHERE id = ?)`
